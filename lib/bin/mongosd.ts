c7529eee6f0d7904d2f735e16df7461c30774bc9
Pelegrinetti <pelegrinetti.dev@gmail.com>
2019-12-16 17:43:48 -0300

    feat: added init script

--- lib/bin/mongosd.ts
+++ lib/bin/mongosd.ts
@@ -1,7 +1,29 @@
 #!/usr/bin/env node
 
-process.argv.map(arg => {
-  if (arg === '--init') {
-    console.log('Initializing')
-  }
-})
+import ora from 'ora'
+import * as path from 'path'
+import fs from 'fs'
+
+const arg = process.argv[2]
+
+if (arg === '--init') {
+  const loader = ora('Initializing...').start()
+
+  setTimeout(() => {
+    loader.text = 'Creating configuration file...'
+    fs.writeFile(
+      path.join(process.cwd(), 'mongosd.json'),
+      `{ modelsPath: '', seedersPath: '', dbUri: '' }`,
+      err => {
+        if (err) {
+          loader.fail()
+          console.log(`> Error: ${err.message}`)
+          process.exit(1)
+        }
+
+        loader.succeed()
+        process.exit(0)
+      }
+    )
+  }, 1000)
+}
