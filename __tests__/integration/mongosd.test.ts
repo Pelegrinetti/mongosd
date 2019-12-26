d03ca36ec91f680cfa8e4d97752194c708fa28a8
Pelegrinetti <pelegrinetti.dev@gmail.com>
2019-12-26 00:51:22 -0300

    fix: command execution

--- __tests__/integration/mongosd.test.ts
+++ __tests__/integration/mongosd.test.ts
@@ -1,4 +1,4 @@
-import { spawn } from 'child_process'
+import { exec } from 'child_process'
 
 import truncate from '../utils/truncate'
 import setMongoSDConfig from '../utils/setMongoSDConfig'
@@ -11,23 +11,16 @@ beforeAll(async done => {
 
 describe('MongoSD tests', () => {
   it('should init mongosd', () => {
-    const result = spawn('node dist/bin/mongosd.js init', { detached: true })
+    const result = exec('node dist/bin/mongosd init')
 
     result.on('exit', async code => {
       await setMongoSDConfig
       expect(code).toBe(0)
-      process.kill(-result.pid)
-    })
-
-    result.on('close', () => {
-      process.kill(-result.pid)
     })
   })
 
   it('should create foo seeder', () => {
-    const result = spawn('node dist/bin/mongosd.js seeder:create foo', {
-      detached: true,
-    })
+    const result = exec('node dist/bin/mongosd seeder:create foo')
 
     result.on('exit', code => {
       expect(code).toBe(0)
@@ -35,16 +28,10 @@ describe('MongoSD tests', () => {
   })
 
   it("shouldn't create foo seeder without seeder name", () => {
-    const result = spawn('node dist/bin/mongosd.js seeder:create', {
-      detached: true,
-    })
+    const result = exec('node dist/bin/mongosd seeder:create')
 
     result.on('exit', code => {
       expect(code).toBe(1)
-      process.kill(-result.pid)
-    })
-
-    result.on('close', () => {
       process.kill(-result.pid)
     })
   })
