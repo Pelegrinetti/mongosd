2623062b35f5983ae6ad7e18a266a4e91f91a9f8
Pelegrinetti <pelegrinetti.dev@gmail.com>
2019-12-26 14:16:55 -0300

    test: added dotenv support

--- __tests__/utils/setMongoSDConfig.ts
+++ __tests__/utils/setMongoSDConfig.ts
@@ -1,14 +1,15 @@
 import fs from 'fs'
 import * as path from 'path'
+import 'dotenv/config'
 
 export default new Promise((resolve, reject) => {
   fs.writeFile(
     path.join(process.cwd(), 'mongosd.json'),
     JSON.stringify(
       {
-        modelsPath: path.join('__tests__', 'case', 'models'),
-        seedersPath: path.join('__tests__', 'case', 'seeders'),
-        dbUri: 'mongodb://localhost/mongosd-test',
+        modelsPath: process.env.MODELS_PATH,
+        seedersPath: process.env.SEEDERS_PATH,
+        dbUri: process.env.DB_URI,
       },
       null,
       2
